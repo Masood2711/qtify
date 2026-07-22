525e738a1f8f54209d066af5cc054ca0351ecf61
Masood2711 <shaikmasoodansarim@gmail.com>
2026-07-23 02:23:40 +0530

    fix: remove unused imports and variables in Search component

--- qtify/src/components/Search/Search.jsx
+++ qtify/src/components/Search/Search.jsx
@@ -4,7 +4,6 @@ import { useAutocomplete } from "@mui/base/useAutocomplete";
 import { styled } from "@mui/system";
 import { truncate } from "../../helpers/helpers";
 import { useNavigate } from "react-router-dom";
-import { Tooltip } from "@mui/material";
 
 const SearchIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
@@ -45,7 +44,6 @@ const Listbox = styled("ul")(({ theme }) => ({
 function Search({ searchData, placeholder }) {
     const {
         getRootProps,
-        getInputLabelProps,
         value,
         getInputProps,
         getListboxProps,
